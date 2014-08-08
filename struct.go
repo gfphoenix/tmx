dffc4de9434b1bb5e5483b9c2c49cce0af66cd25
Steve McCoy <mccoyst@gmail.com>
2014-08-08 19:15:30 -0400

    Add Decode function to handle Layers

--- struct.go
+++ struct.go
@@ -6,36 +6,67 @@ https://github.com/bjorn/tiled/wiki/TMX-Map-Format .
 */
 package tmx
 
-type Map struct {
-	XMLName xml.Name `xml:"map"`
-	Version string `xml:"version,attr"`
-	Orientation string `xml:"orientation,attr"`
-	Width int `xml:"width,attr"`
-	Height int `xml:"height,attr"`
-	TileWidth int `xml:"tilewidth,attr"`
-	TileHeight int `xml:"tileheight,attr"`
-	BackgroundColor string `xml:"backgroundcolor,attr"`
+import (
+	"bufio"
+	"bytes"
+	"compress/gzip"
+	"compress/zlib"
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/xml"
+	"io"
+	"strconv"
+	"strings"
+)
 
-	Properties []Property `xml:"properties>property"`
-	Tilesets []Tileset `xml:"tileset"`
-	Layers []Layer `xml:"layer"`
-	ObjectGroups []ObjectGroup `xml:"objectgroup"`
-	ImageLayers []ImageLayer `xml:"imagelayer"`
+func Decode(r io.Reader) (*Map, error) {
+	m := new(Map)
+	x := xml.NewDecoder(r)
+	err := x.Decode(m)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range m.Layers {
+		err = m.Layers[i].decodeIDs()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return m, nil
 }
 
-type TileSet struct {
-	FirstGID int32 `xml:"firstgid,attr"`
-	Source string `xml:"source,attr"`
-	Name string `xml:"name,attr"`
-	TileWidth int `xml:"tilewidth,attr"`
-	TileHeight int `xml:"tileheight,attr"`
-	Spacing int `xml:"spacing,attr"`
-	Margin int `xml:"margin,attr"`
+type Map struct {
+	XMLName         xml.Name `xml:"map"`
+	Version         string   `xml:"version,attr"`
+	Orientation     string   `xml:"orientation,attr"`
+	Width           int      `xml:"width,attr"`
+	Height          int      `xml:"height,attr"`
+	TileWidth       int      `xml:"tilewidth,attr"`
+	TileHeight      int      `xml:"tileheight,attr"`
+	BackgroundColor string   `xml:"backgroundcolor,attr"`
 
-	TileOffset TileOffset `xml:"tileoffset"`
-	Properties []Property `xml:"properties>property"`
-	Image Image `xml:"image"`
-	TerrainTypes []Terrain `xml:"terraintypes>terrain"`
+	Properties   []Property    `xml:"properties>property"`
+	Tilesets     []Tileset     `xml:"tileset"`
+	Layers       []Layer       `xml:"layer"`
+	ObjectGroups []ObjectGroup `xml:"objectgroup"`
+	ImageLayers  []ImageLayer  `xml:"imagelayer"`
+}
+
+type Tileset struct {
+	FirstGID   int32  `xml:"firstgid,attr"`
+	Source     string `xml:"source,attr"`
+	Name       string `xml:"name,attr"`
+	TileWidth  int    `xml:"tilewidth,attr"`
+	TileHeight int    `xml:"tileheight,attr"`
+	Spacing    int    `xml:"spacing,attr"`
+	Margin     int    `xml:"margin,attr"`
+
+	TileOffset   TileOffset `xml:"tileoffset"`
+	Properties   []Property `xml:"properties>property"`
+	Image        Image      `xml:"image"`
+	TerrainTypes []Terrain  `xml:"terraintypes>terrain"`
 }
 
 type TileOffset struct {
@@ -46,43 +77,102 @@ type TileOffset struct {
 type Image struct {
 	Format string `xml:"format,attr"`
 	Source string `xml:"source,attr"`
-	Trans string `xml:"trans,attr"`
-	Width int `xml:"width,attr"`
-	Height int `xml:"height,attr"`
+	Trans  string `xml:"trans,attr"`
+	Width  int    `xml:"width,attr"`
+	Height int    `xml:"height,attr"`
 
 	Data Data `xml:"data"`
 }
 
 type Terrain struct {
 	Name string `xml:"name,attr"`
-	Tile int `xml:"tile,attr"`
+	Tile int    `xml:"tile,attr"`
 
 	Properties []Property `xml:"properties>property"`
 }
 
 type Tile struct {
-	ID int32 `xml:"id,attr"`
-	Terrain string `xml:"terrain,attr"`
+	ID          int32   `xml:"id,attr"`
+	Terrain     string  `xml:"terrain,attr"`
 	Probability float32 `xml:"probability,attr"`
 
 	Properties []Property `xml:"properties>property"`
-	Image Image `xml:"image"`
+	Image      Image      `xml:"image"`
 }
 
 type Layer struct {
-	Name string `xml:"name,attr"`
+	Name    string  `xml:"name,attr"`
 	Opacity float32 `xml:"opacity,attr"`
-	Visible bool `xml:"visible,attr"`
+	Visible bool    `xml:"visible,attr"`
 
 	Properties []Property `xml:"properties>property"`
-	Data Data `xml:"data"`
+	Data       Data       `xml:"data"`
+
+	// The GID of each tile, in order. Use this instead of raw Data, which is cleaned up by Decode.
+	GIDs []int32 `xml:"-"`
+}
+
+func (l *Layer) decodeIDs() error {
+	d := &l.Data
+	if d.Encoding == "csv" {
+		r := bufio.NewScanner(strings.NewReader(d.Text))
+		for r.Scan() {
+			line := r.Text()
+			parts := strings.Split(line, ",")
+			for _, p := range parts {
+				if p == "" {
+					continue
+				}
+				n, err := strconv.ParseInt(p, 10, 32)
+				if err != nil {
+					return err
+				}
+				l.GIDs = append(l.GIDs, int32(n))
+			}
+		}
+		if err := r.Err(); err != nil {
+			return err
+		}
+	} else if d.Encoding == "base64" {
+		raw, err := base64.StdEncoding.DecodeString(strings.TrimSpace(d.Text))
+		if err != nil {
+			return err
+		}
+		var r io.Reader = bytes.NewReader(raw)
+		if d.Compression == "zlib" {
+			r, err = zlib.NewReader(r)
+		} else if d.Compression == "gzip" {
+			r, err = gzip.NewReader(r)
+		}
+		if err != nil {
+			return err
+		}
+		for {
+			var n int32
+			err = binary.Read(r, binary.LittleEndian, &n)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				return err
+			}
+			l.GIDs = append(l.GIDs, n)
+		}
+	} else {
+		for i := range d.Tiles {
+			l.GIDs = append(l.GIDs, d.Tiles[i].GID)
+		}
+	}
+
+	l.Data = Data{}
+	return nil
 }
 
 type Data struct {
-	Encoding string `xml:"encoding,attr"`
+	Encoding    string `xml:"encoding,attr"`
 	Compression string `xml:"compression,attr"`
 
-	Text []byte `xml:",chardata"`
+	Text  string       `xml:",chardata"`
 	Tiles []SingleTile `xml:"tile"`
 }
 
@@ -90,45 +180,45 @@ type SingleTile struct {
 	GID int32 `xml:"gid,attr"`
 }
 
-type ObjectGroup struct [
-	Name string `xml:"name,attr"`
-	Color string `xml:"color,attr"`
+type ObjectGroup struct {
+	Name    string  `xml:"name,attr"`
+	Color   string  `xml:"color,attr"`
 	Opacity float32 `xml:"opacity,attr"`
-	Visible bool `xml:"visible,attr"`
+	Visible bool    `xml:"visible,attr"`
 
 	Properties []Property `xml:"properties>property"`
-	Objects []Object `xml:"objects"`
+	Objects    []Object   `xml:"object"`
 }
 
 type Object struct {
-	Name string `xml:"name,attr"`
-	Type string `xml:"type,attr"`
-	X int `xml:"x,attr"`
-	Y int `xml:"y,attr"`
-	Width int `xml:"width,attr"`
-	Height int `xml:"height,attr"`
+	Name     string  `xml:"name,attr"`
+	Type     string  `xml:"type,attr"`
+	X        int     `xml:"x,attr"`
+	Y        int     `xml:"y,attr"`
+	Width    int     `xml:"width,attr"`
+	Height   int     `xml:"height,attr"`
 	Rotation float32 `xml:"rotation,attr"`
-	GID int32 `xml:"gid,attr"`
-	Visible bool `xml:"visible,attr"`
+	GID      int32   `xml:"gid,attr"`
+	Visible  bool    `xml:"visible,attr"`
 
 	Properties []Property `xml:"properties>property"`
-	Ellipse *Ellipse `xml:"ellipse"`
-	Polygon string `xml:"polygon>points"`
-	Polylines string `xml:"polyline>points"`
+	Ellipse    *Ellipse   `xml:"ellipse"`
+	Polygon    string     `xml:"polygon>points"`
+	Polylines  string     `xml:"polyline>points"`
 }
 
 type Ellipse struct{}
 
 type ImageLayer struct {
-	Name string `xml:"name,attr"`
+	Name    string  `xml:"name,attr"`
 	Opacity float32 `xml:"opacity,attr"`
-	Visible bool `xml:"visible,attr"`
+	Visible bool    `xml:"visible,attr"`
 
 	Properties []Property `xml:"properties>property"`
-	Image Image `xml:"image"`
+	Image      Image      `xml:"image"`
 }
 
 type Property struct {
-	Name string `xml:"name,attr"`
+	Name  string `xml:"name,attr"`
 	Value string `xml:"value,attr"`
 }
---
+++ struct_test.go
@@ -0,0 +1,271 @@
+// © 2014 Steve McCoy under the MIT license. See LICENSE for details.
+
+package tmx
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/eaburns/eq"
+)
+
+func TestDecode(t *testing.T) {
+	var maps []*Map
+
+	for i, s := range []string{ testXml, testCsv, testBase64, testGzip, testZlib } {
+		m, err := Decode(strings.NewReader(s))
+		if err != nil {
+			t.Fatalf("unexpected decode error for %d: %v", i, err)
+		}
+		maps = append(maps, m)
+	}
+
+	for i := 1; i < len(maps); i++ {
+		if !eq.Deep(maps[i-1], maps[i]) {
+			t.Fatalf("unequal %d vs. %d:\n%v\n------\n%v", i-1, i, maps[i-1], maps[i])
+		}
+	}
+}
+
+var testXml = `
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0" orientation="orthogonal" width="10" height="10" tilewidth="16" tileheight="16">
+ <tileset firstgid="1" name="land" tilewidth="16" tileheight="16">
+  <image source="tiles.png" width="48" height="16"/>
+ </tileset>
+ <layer name="Foreground" width="10" height="10">
+  <data>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="2"/>
+   <tile gid="2"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+   <tile gid="2"/>
+   <tile gid="2"/>
+   <tile gid="2"/>
+   <tile gid="2"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="2"/>
+   <tile gid="2"/>
+   <tile gid="2"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="2"/>
+   <tile gid="2"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+   <tile gid="3"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="1"/>
+   <tile gid="3"/>
+  </data>
+ </layer>
+ <objectgroup name="Mountains" width="10" height="10">
+  <object type="mountain" x="48" y="48" width="16" height="16"/>
+  <object type="mountain" x="64" y="32" width="16" height="16"/>
+  <object type="mountain" x="80" y="32" width="16" height="16"/>
+  <object type="mountain" x="64" y="48" width="16" height="16"/>
+  <object type="mountain" x="96" y="48" width="16" height="16"/>
+  <object type="mountain" x="80" y="48" width="16" height="16"/>
+  <object type="mountain" x="64" y="64" width="16" height="16"/>
+  <object type="mountain" x="80" y="64" width="16" height="16"/>
+  <object type="mountain" x="96" y="64" width="16" height="16"/>
+  <object type="mountain" x="80" y="80" width="16" height="16"/>
+  <object type="mountain" x="96" y="80" width="16" height="16"/>
+ </objectgroup>
+</map>
+`
+
+var testCsv = `
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0" orientation="orthogonal" width="10" height="10" tilewidth="16" tileheight="16">
+ <tileset firstgid="1" name="land" tilewidth="16" tileheight="16">
+  <image source="tiles.png" width="48" height="16"/>
+ </tileset>
+ <layer name="Foreground" width="10" height="10">
+  <data encoding="csv">
+3,3,1,1,1,1,1,1,1,3,
+3,1,1,3,3,3,3,1,1,1,
+1,1,3,3,2,2,3,3,1,1,
+1,1,3,2,2,2,2,3,1,1,
+1,1,3,3,2,2,2,3,1,1,
+1,1,3,3,3,2,2,3,1,1,
+1,1,3,3,3,3,3,3,3,1,
+3,1,1,3,3,3,3,3,3,1,
+3,1,1,1,1,1,3,3,1,1,
+3,3,1,1,1,1,1,1,1,3
+</data>
+ </layer>
+ <objectgroup name="Mountains" width="10" height="10">
+  <object type="mountain" x="48" y="48" width="16" height="16"/>
+  <object type="mountain" x="64" y="32" width="16" height="16"/>
+  <object type="mountain" x="80" y="32" width="16" height="16"/>
+  <object type="mountain" x="64" y="48" width="16" height="16"/>
+  <object type="mountain" x="96" y="48" width="16" height="16"/>
+  <object type="mountain" x="80" y="48" width="16" height="16"/>
+  <object type="mountain" x="64" y="64" width="16" height="16"/>
+  <object type="mountain" x="80" y="64" width="16" height="16"/>
+  <object type="mountain" x="96" y="64" width="16" height="16"/>
+  <object type="mountain" x="80" y="80" width="16" height="16"/>
+  <object type="mountain" x="96" y="80" width="16" height="16"/>
+ </objectgroup>
+</map>
+`
+
+var testBase64 = `
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0" orientation="orthogonal" width="10" height="10" tilewidth="16" tileheight="16">
+ <tileset firstgid="1" name="land" tilewidth="16" tileheight="16">
+  <image source="tiles.png" width="48" height="16"/>
+ </tileset>
+ <layer name="Foreground" width="10" height="10">
+  <data encoding="base64">
+   AwAAAAMAAAABAAAAAQAAAAEAAAABAAAAAQAAAAEAAAABAAAAAwAAAAMAAAABAAAAAQAAAAMAAAADAAAAAwAAAAMAAAABAAAAAQAAAAEAAAABAAAAAQAAAAMAAAADAAAAAgAAAAIAAAADAAAAAwAAAAEAAAABAAAAAQAAAAEAAAADAAAAAgAAAAIAAAACAAAAAgAAAAMAAAABAAAAAQAAAAEAAAABAAAAAwAAAAMAAAACAAAAAgAAAAIAAAADAAAAAQAAAAEAAAABAAAAAQAAAAMAAAADAAAAAwAAAAIAAAACAAAAAwAAAAEAAAABAAAAAQAAAAEAAAADAAAAAwAAAAMAAAADAAAAAwAAAAMAAAADAAAAAQAAAAMAAAABAAAAAQAAAAMAAAADAAAAAwAAAAMAAAADAAAAAwAAAAEAAAADAAAAAQAAAAEAAAABAAAAAQAAAAEAAAADAAAAAwAAAAEAAAABAAAAAwAAAAMAAAABAAAAAQAAAAEAAAABAAAAAQAAAAEAAAABAAAAAwAAAA==
+  </data>
+ </layer>
+ <objectgroup name="Mountains" width="10" height="10">
+  <object type="mountain" x="48" y="48" width="16" height="16"/>
+  <object type="mountain" x="64" y="32" width="16" height="16"/>
+  <object type="mountain" x="80" y="32" width="16" height="16"/>
+  <object type="mountain" x="64" y="48" width="16" height="16"/>
+  <object type="mountain" x="96" y="48" width="16" height="16"/>
+  <object type="mountain" x="80" y="48" width="16" height="16"/>
+  <object type="mountain" x="64" y="64" width="16" height="16"/>
+  <object type="mountain" x="80" y="64" width="16" height="16"/>
+  <object type="mountain" x="96" y="64" width="16" height="16"/>
+  <object type="mountain" x="80" y="80" width="16" height="16"/>
+  <object type="mountain" x="96" y="80" width="16" height="16"/>
+ </objectgroup>
+</map>
+`
+
+var testGzip = `
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0" orientation="orthogonal" width="10" height="10" tilewidth="16" tileheight="16">
+ <tileset firstgid="1" name="land" tilewidth="16" tileheight="16">
+  <image source="tiles.png" width="48" height="16"/>
+ </tileset>
+ <layer name="Foreground" width="10" height="10">
+  <data encoding="base64" compression="gzip">
+   H4sIAAAAAAAAA2NmYGBgBmJGPJgZTQ0zGsanhwmKcalFVoOslpB5hNSh241PHTb/4PMvLnXEhBmhcAYABg1fRZABAAA=
+  </data>
+ </layer>
+ <objectgroup name="Mountains" width="10" height="10">
+  <object type="mountain" x="48" y="48" width="16" height="16"/>
+  <object type="mountain" x="64" y="32" width="16" height="16"/>
+  <object type="mountain" x="80" y="32" width="16" height="16"/>
+  <object type="mountain" x="64" y="48" width="16" height="16"/>
+  <object type="mountain" x="96" y="48" width="16" height="16"/>
+  <object type="mountain" x="80" y="48" width="16" height="16"/>
+  <object type="mountain" x="64" y="64" width="16" height="16"/>
+  <object type="mountain" x="80" y="64" width="16" height="16"/>
+  <object type="mountain" x="96" y="64" width="16" height="16"/>
+  <object type="mountain" x="80" y="80" width="16" height="16"/>
+  <object type="mountain" x="96" y="80" width="16" height="16"/>
+ </objectgroup>
+</map>
+`
+
+var testZlib = `
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0" orientation="orthogonal" width="10" height="10" tilewidth="16" tileheight="16">
+ <tileset firstgid="1" name="land" tilewidth="16" tileheight="16">
+  <image source="tiles.png" width="48" height="16"/>
+ </tileset>
+ <layer name="Foreground" width="10" height="10">
+  <data encoding="base64" compression="zlib">
+   eJxjZmBgYAZiRjyYGU0NMxrGp4cJinGpRVaDrJaQeYTUoduNTx02/+DzLy51xIQZoXAGAJlEAMI=
+  </data>
+ </layer>
+ <objectgroup name="Mountains" width="10" height="10">
+  <object type="mountain" x="48" y="48" width="16" height="16"/>
+  <object type="mountain" x="64" y="32" width="16" height="16"/>
+  <object type="mountain" x="80" y="32" width="16" height="16"/>
+  <object type="mountain" x="64" y="48" width="16" height="16"/>
+  <object type="mountain" x="96" y="48" width="16" height="16"/>
+  <object type="mountain" x="80" y="48" width="16" height="16"/>
+  <object type="mountain" x="64" y="64" width="16" height="16"/>
+  <object type="mountain" x="80" y="64" width="16" height="16"/>
+  <object type="mountain" x="96" y="64" width="16" height="16"/>
+  <object type="mountain" x="80" y="80" width="16" height="16"/>
+  <object type="mountain" x="96" y="80" width="16" height="16"/>
+ </objectgroup>
+</map>
+`
